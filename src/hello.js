e4c0288ff8652c9088e5224f85e971d6a31e6713
harrylee02 <ngochieu0211.study@gmail.com>
2024-05-26 23:30:59 +0700

    change js

--- src/hello.js
+++ src/hello.js
@@ -2,11 +2,13 @@
 function checkName() {
     const userInput = document.getElementById('name').value;
     const expectedName = 'Nguyen Tran Bao Nhi'; // Replace with your desired string
-
-    if (userInput === expectedName) {
-        // Redirect to another page (e.g., 'page3.html')
-        window.location.href = 'hello.html';
-    } else {
-        $('#exampleModal').modal('show'); // Show an alert if the input doesn't match
+    if (userInput)
+    {
+        if (userInput === expectedName) {
+            // Redirect to another page (e.g., 'page3.html')
+            window.location.href = 'hello.html';
+        } else {
+            $('#exampleModal').modal('show'); // Show an alert if the input doesn't match
+        }
     }
 }
