712ca42e9589991a105dbf84a09e640b5d3a1813
harrylee02 <ngochieu0211.study@gmail.com>
2024-05-28 21:40:51 +0700

    change

--- src/hello.js
+++ src/hello.js
@@ -1,24 +1,24 @@
 // script.js
 
-const nameArr = {
-    "Nguyen Tran Bao Nhi": "26-05-2004",
-    "Le Ngoc Hieu": "02-11-2004"
-  };
+const birthdayMap = new Map();
+birthdayMap.set("Nguyen Tran Bao Nhi", "26-05-2004");
+birthdayMap.set("Le Ngoc Hieu", "02-11-2004");
+
 
 function checkName() {
     const userInput = document.getElementById('name').value;
-    if (userInput !== '') {
-        for (const name in nameArr) {
-            if (userInput === name) {
-                window.location.href = '/hello.html';
-                flag = true;
-                break;
-            }
+    let flag = false;
+    for (const [key,value] of birthdayMap) {
+        if (userInput === key) {
+            window.location.href = '/hello.html';
+            flag = true;
+            break;
         }
-    } else {
+    }
+    if (flag === false) {
         $('.modal_title_placeholder').html('Thông báo!')
         $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)')
-        $('#exampleModal').modal('show'); // Show an alert if the input doesn't match
+        $('#Modal').modal('show'); // Show an alert if the input doesn't match
     }
 }
 
