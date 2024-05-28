4d80a80de66960b3470119fc6e24e2d93e05ccc6
harrylee02 <ngochieu0211.study@gmail.com>
2024-05-28 22:19:55 +0700

    up

--- src/hello.js
+++ src/hello.js
@@ -4,20 +4,32 @@ const birthdayMap = new Map();
 birthdayMap.set("Nguyen Tran Bao Nhi", "26-05-2004");
 birthdayMap.set("Le Ngoc Hieu", "02-11-2004");
 
+const nameForm = document.getElementById("nameForm");
+
+    // Attach an event listener to the form
+    nameForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent default form submission
+        checkName(); // Call your custom function
+    });
 
 function checkName() {
-    const userInput = document.getElementById('name').value;
-    for (const [key,value] of birthdayMap) {
-        if (userInput == key) {
+    const userInput = document.getElementById("name").value;
+    let flag = false; // Initialize flag to false
+
+    for (const [key, value] of birthdayMap) {
+        if (userInput === key) {
             flag = true;
             window.location.href = '/hello.html';
             break;
         }
     }
-    $('.modal_title_placeholder').html('Thông báo!')
-    $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)')
-    $('#Modal').modal('show'); // Show an alert if the input doesn't match
+
+    if (flag == false) {
+        // Show the modal when the input doesn't match
+        $('.modal_title_placeholder').html('Thông báo!');
+        $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)');
+        $('#Modal').modal('show');
+    }
 }
-document.getElementById("checkName").onclick = checkName();
 
 
