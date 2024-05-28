a49c0b2749dfd6100e6cf1668ebe199bb3fcf180
harrylee02 <ngochieu0211.study@gmail.com>
2024-05-28 21:16:19 +0700

    add gifs

--- src/hello.js
+++ src/hello.js
@@ -1,14 +1,25 @@
 // script.js
+
+const nameArr = {
+    "Nguyen Tran Bao Nhi": "26-05-2004",
+    "Le Ngoc Hieu": "02-11-2004"
+  };
+
 function checkName() {
     const userInput = document.getElementById('name').value;
-    const expectedName = 'Nguyen Tran Bao Nhi'; // Replace with your desired string
-    if (userInput !== '' && userInput === expectedName) {
-    
-            // Redirect to another page (e.g., 'page3.html')
-            window.location.href = '/hello.html';
-        } else {
-            $('#exampleModal').modal('show'); // Show an alert if the input doesn't match
+    if (userInput !== '') {
+        for (const name in nameArr) {
+            if (userInput === name) {
+                window.location.href = '/hello.html';
+                flag = true;
+                break;
+            }
         }
-    
+    } else {
+        $('.modal_title_placeholder').html('Thông báo!')
+        $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)')
+        $('#exampleModal').modal('show'); // Show an alert if the input doesn't match
+    }
 }
+
 
