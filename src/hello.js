b1d4573c1a284041e219a2ee60beacc8acbff6ca
harrylee02 <ngochieu0211.study@gmail.com>
2024-05-28 21:48:21 +0700

    update

--- src/hello.js
+++ src/hello.js
@@ -7,19 +7,17 @@ birthdayMap.set("Le Ngoc Hieu", "02-11-2004");
 
 function checkName() {
     const userInput = document.getElementById('name').value;
-    let flag = false;
     for (const [key,value] of birthdayMap) {
-        if (userInput === key) {
-            window.location.href = '/hello.html';
+        if (userInput == key) {
             flag = true;
+            window.location.href = '/hello.html';
             break;
         }
     }
-    if (flag === false) {
-        $('.modal_title_placeholder').html('Thông báo!')
-        $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)')
-        $('#Modal').modal('show'); // Show an alert if the input doesn't match
-    }
+    $('.modal_title_placeholder').html('Thông báo!')
+    $('.modal_body_placeholder').html('You are not on the list, get out! (bro sneak into a stranger github or cannot write the name right, damn... >_<)')
+    $('#Modal').modal('show'); // Show an alert if the input doesn't match
 }
+document.getElementById("checkName").onclick = checkName();
 
 
